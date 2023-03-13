5b7a4439af7ede4d8078c80daf7476625eee5341
Daniel-acFerraz <daniel.albinocf@gmail.com>
2023-03-13 16:52:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function rot13(str) {
-  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  let answer = [];
-  let strArray = str.split(""); 
-  for(let i = 0; i < strArray.length; i++){
-    if(alphabet.indexOf(strArray[i]) === -1){
-      answer.push(strArray[i])
-    }else if(alphabet.indexOf(strArray[i]) > 12){    
-      answer.push(alphabet[alphabet.indexOf(strArray[i])-13])
-    }else if(alphabet.indexOf(strArray) <= 12) {
-      answer.push(alphabet[alphabet.indexOf(strArray[i])+13])
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let answer = "";
+  for(let i = 0; i < str.length; i++){
+    if(alphabet.indexOf(str[i]) === -1){
+      answer += str[i]
+    }else if(alphabet.indexOf(str[i]) > 12){    
+      answer += alphabet[alphabet.indexOf(str[i])-13]
+    }else if(alphabet.indexOf(str) <= 12) {
+      answer += alphabet[alphabet.indexOf(str[i])+13]
     }
-  }
-  
-  return answer.join("");
+  }  
+  return answer;
 }
